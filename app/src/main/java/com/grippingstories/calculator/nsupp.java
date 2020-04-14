72030a11c1a0caba119cb9677c89ff4dd49bf8b9
sankalp04 <sankalpgarg2012@gmail.com>
2020-04-14 22:49:33 +0530

    this is for basic learning.

---
+++ app/src/main/java/com/grippingstories/calculator/nsupp.java
@@ -0,0 +1,13 @@
+package com.grippingstories.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nsupp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nsupp);
+    }
+}
